24a96a460ac0cee3fbb4ddeb99b43874bc7d19ff
admin <admin@DESKTOP-GKKSMO3>
2025-09-28 13:27:15 +0500

    Task2

---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint1.Task2.V5.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint1.Task2.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds=new DataService();
+            int x = 2;
+            var res = ds.CalculateSideSquare(x);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
