6372977faeaf9d7dca6f921d9aceef45937c4693
admin <admin@DESKTOP-GKKSMO3>
2025-09-29 13:46:27 +0500

    Task4

---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint1.Task4.V6.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint1.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.5, res);
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task4.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint1.Task4.V6.Lib;
+
+
+//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//вычисляет результат по формуле и печатает его на экране.
+//Формула:
+//  |y ^ 2 - x|
+//  -----------
+//    x * y
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" РЕЗУЛЬТАТ:                                                                ");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(" |y ^ 2 - x| / (x * y) = " + ds.Calculate(x, y));
+
+        Console.ReadKey();
+
+
+    }
+}
