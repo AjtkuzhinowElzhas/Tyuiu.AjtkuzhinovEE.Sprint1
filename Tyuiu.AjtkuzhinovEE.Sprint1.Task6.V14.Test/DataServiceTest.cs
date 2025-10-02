323fbfe5c0be37f331a2adcd0b54bbbfc49cfa72
admin <admin@DESKTOP-GKKSMO3>
2025-10-03 00:37:19 +0500

    Task6

---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint1.Task6.V14.Lib;
+
+
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint1.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            char[] russianLetters = new char[32];
+            for (int i = 0; i < 32; i++)
+            {
+                russianLetters[i] = (char)('?' + i);
+            }
+            string strTest = "???????????????????";
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.CheckLowerCaseRusLetters(strTest));
+
+
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task6.V14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using Tyuiu.AjtkuzhinovEE.Sprint1.Task6.V14.Lib;
+
+//ЗАДАНИЕ
+//Написать программу: пользователь вводит текст.
+//Проверить, что строка составлена только из строчных русских букв.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value;
+
+        Console.WriteLine("Введите строку:");
+        value = Convert.ToString(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine($"{ds.CheckLowerCaseRusLetters(value)}");
+
+        Console.ReadLine();
+
+    }
+
+
+
+}
