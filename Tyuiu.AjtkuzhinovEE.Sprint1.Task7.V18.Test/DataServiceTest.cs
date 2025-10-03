00c0bfa3091057345597f07ad2101a88fb970427
admin <admin@DESKTOP-GKKSMO3>
2025-10-03 12:52:11 +0500

    Task7

---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.WebSockets;
+using Tyuiu.AjtkuzhinovEE.Sprint1.Task7.V18.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 30;
+            double y = 0;
+            double wait = 30.062;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint1.Task7.V18/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint1.Task7.V18.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая вычисляет математическое выражение
+//по исходным значениям данных, вводимых пользователем.
+//Ответ округлите до 3 знаков после запятой.
+//
+//
+//               1 + sin ^ 2 (x + y)
+// z = ------------------------------------------  + x
+//           |           2 * x           |
+//       2 + | x - --------------------- |
+//           |      1 + x ^ 2 * y ^ 2    |
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Айткужинов Е. Е. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Айткужинов Елжас Есилбаевич | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                1+sin ^ 2(x+y)                           *");
+        Console.WriteLine("*                        z= --------------------- +x                      *");
+        Console.WriteLine("*                               |       2x    |                           *");
+        Console.WriteLine("*                            2+ | x- ---------|                           *");
+        Console.WriteLine("*                               |    1+x^2*y^2|                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+        Console.WriteLine("Введите значение x:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadKey();
+    }
+}
